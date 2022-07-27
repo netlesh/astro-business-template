9f48c6a585a953195f29cf227f07ef488cc81458
netlesh <netlesh@mail.ru>
2022-07-27 15:14:38 +0300

    create 2 files and update 3 files

---
+++ src/config/settings.js
@@ -0,0 +1,6 @@
+export default {
+	title: `ŞİRKET Sitesi`,
+	description: `Temiz bir web şablonu`,
+	url: `https://cemkaan.com`, // No trailing slash!
+	name: `Şirket`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+};
