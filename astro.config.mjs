a41b84736a2beea0e8b34558325f278355a70f45
netlesh <netlesh@mail.ru>
2022-07-29 21:47:11 +0300

    update 5 files and delete 8 files

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
   // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
   site: "https://cemkaan.com",
   integrations: [
