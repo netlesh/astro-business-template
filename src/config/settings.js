f6ebee0c25a9797dde8f2db86f0f1453956ca09e
netlesh <netlesh@mail.ru>
2022-07-30 17:51:35 +0300

    update 5 files and create 1 file

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,4 +3,5 @@ export default {
 	description: `Temiz bir web şablonu`,
 	url: `https://cemkaan.com`, // No trailing slash!
 	name: `Şirket`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	adres: `Voyvoda Caddesi (Bankalar Caddesi) No: 35-37 Karaköy Beyoğlu İstanbul`
 };
